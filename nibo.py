2aa894cdd2faefe9823061bc997a0eb2c596ee60
Volker von Einem <vve@gmx.de>
2019-02-07 20:05:31 +0100

    Better thread management + response handling

--- nibo.py
+++ nibo.py
@@ -1,24 +1,48 @@
 import serial
 import time
-from threading import Thread
+import threading
 
 ser = serial.Serial('/dev/serial0', 9600)
+ser.timeout = 1
+stopReadThread = threading.Event()
+responseReceived = threading.Event()
+response = []
+
+def ReadThread(waitTime):
+        global stopReadThread
+        while(stopReadThread.is_set() != True):
+                sbyte = ser.read()                
+                print(sbyte.decode(), end='', flush=True)
+
+                if sbyte == b'\r' or sbyte == b'\n':
+                    responseReceived.set()
+                else :
+                    response.append(sbyte)
+
+                stopReadThread.wait(waitTime)
+
+reader = threading.Thread(target = ReadThread, args= (0.001, ))
+
+def Start():
+        reader.start()
+        time.sleep(0.1)
+
+def Stop():
+        global stopReadThread
+        stopReadThread.set()
 
 def Delay(milliseconds):
     time.sleep(milliseconds / 1000.0)
 
-def ReadThread(waitTime):
-    while(1):
-        sbyte = ser.read()
-        print(sbyte.decode(), end='', flush=True)
-        time.sleep(waitTime)
-
 def Send(command):
-    # print("sending: " + command)
+    global response
+    response = []
+    responseReceived.clear()
     ser.write(command.encode('utf-8'))
     ser.write(b'\r\n')
+    responseReceived.wait(5)
+    return ''.join([x.decode() for x in response])
 
-reader = Thread(target = ReadThread, args= (0.001, ))
-reader.start()
-
-time.sleep(1)
+def GetSupplyVoltageMillivolt():
+    # TODO
+    Send('')
--- supply_voltage.py
+++ supply_voltage.py
@@ -1,5 +1,10 @@
-from nibo import *
+import nibo
 
-while(1):
-    Send('request get 2')
-    Delay(1000)
+nibo.Start()
+
+for i in range(1, 4):
+    response = nibo.Send('request get 2')
+    print("response: '{r}'".format(r = response))
+    nibo.Delay(1000)
+
+nibo.Stop()
