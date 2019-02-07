23619ce07acde99e59b1bfb45edde4a64e1260cc
Volker von Einem <vve@gmx.de>
2019-02-07 21:28:51 +0100

    Bugfixes and enhancements

--- nibo.py
+++ nibo.py
@@ -5,19 +5,30 @@ import threading
 ser = serial.Serial('/dev/serial0', 9600)
 ser.timeout = 1
 stopReadThread = threading.Event()
+echoReceived = threading.Event()
 responseReceived = threading.Event()
+echo = []
 response = []
 
 def ReadThread(waitTime):
         global stopReadThread
+        global echoReceived
+        global responseReceived
+        global response
         while(stopReadThread.is_set() != True):
                 sbyte = ser.read()                
-                print(sbyte.decode(), end='', flush=True)
+                # print(sbyte.decode('utf-8'), end='', flush=True)
 
-                if sbyte == b'\r' or sbyte == b'\n':
-                    responseReceived.set()
-                else :
-                    response.append(sbyte)
+                if echoReceived.is_set() == False:
+                        if sbyte == b'\n':
+                                echoReceived.set()
+                        else :
+                                if sbyte != b'\r' : echo.append(sbyte)
+                else:                        
+                        if sbyte == b'\n':
+                                responseReceived.set()
+                        else :
+                                if sbyte != b'\r' : response.append(sbyte)
 
                 stopReadThread.wait(waitTime)
 
@@ -35,14 +46,25 @@ def Delay(milliseconds):
     time.sleep(milliseconds / 1000.0)
 
 def Send(command):
+    global echoReceived
+    global responseReceived
     global response
+    global echo
+    echo = []
     response = []
+    echoReceived.clear()
     responseReceived.clear()
+    print('<<', command)
     ser.write(command.encode('utf-8'))
     ser.write(b'\r\n')
+    echoReceived.wait(5)
+    # sEcho = ''.join([x.decode('utf-8') for x in echo])
+    # print('echo: ', sEcho)
     responseReceived.wait(5)
-    return ''.join([x.decode() for x in response])
+    sResponse = ''.join([x.decode('utf-8') for x in response])
+    print('>>', sResponse)
+    return sResponse
 
 def GetSupplyVoltageMillivolt():
-    # TODO
-    Send('')
+    respone = Send('request get 2')
+    return respone
--- supply_voltage.py
+++ supply_voltage.py
@@ -3,7 +3,7 @@ import nibo
 nibo.Start()
 
 for i in range(1, 4):
-    response = nibo.Send('request get 2')
+    response = nibo.GetSupplyVoltageMillivolt()
     print("response: '{r}'".format(r = response))
     nibo.Delay(1000)
 
