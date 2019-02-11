7753841d0b1f24c825be762a080ed68b00d767e8
Volker von Einem <vve@gmx.de>
2019-02-12 00:39:45 +0100

    Added video stream

--- ball_tracking_pi.py
+++ ball_tracking_pi.py
@@ -2,6 +2,9 @@
 # https://www.pyimagesearch.com/2015/09/14/ball-tracking-with-opencv/
 # https://www.pyimagesearch.com/2015/03/30/accessing-the-raspberry-pi-camera-with-opencv-and-python/
 #
+# For the video stream
+# https://blog.miguelgrinberg.com/post/video-streaming-with-flask
+#
 # Goal is to let the NIBOBee follow a green ball - first step is get the deviation from the middle
 
 from collections import deque
@@ -13,76 +16,117 @@ import imutils
 import time
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+from importlib import import_module
+import os
+from flask import Flask, render_template, Response
+import threading
+import io
 
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
-greenLower = (29, 86, 6)
-greenUpper = (64, 255, 255)
+webFrame = None
 
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
+def ImageProcessingThread(dummy):
+    global webFrame
+    # define the lower and upper boundaries of the "green"
+    # ball in the HSV color space, then initialize the
+    # list of tracked points
+    greenLower = (29, 86, 6)
+    greenUpper = (64, 255, 255)
 
-# allow the camera or video file to warm up
+    # initialize the camera and grab a reference to the raw camera capture
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    camera.framerate = 32
+    rawCapture = PiRGBArray(camera, size=(640, 480))
+    
+    # allow the camera or video file to warm up
+    time.sleep(0.1)
+
+    # capture frames from the camera
+    for image in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+            # grab the raw NumPy array representing the image, then initialize the timestamp
+            # and occupied/unoccupied text
+        frame = image.array
+        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+        # construct a mask for the color "green", then perform
+        # a series of dilations and erosions to remove any small
+        # blobs left in the mask
+        mask = cv2.inRange(hsv, greenLower, greenUpper)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+
+    #	cv2.imshow("mask", mask)
+
+        # find contours in the mask and initialize the current
+        # (x, y) center of the ball
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        center = None
+
+        # only proceed if at least one contour was found
+        if len(cnts) > 0:
+                    # find the largest contour in the mask, then use
+                    # it to compute the minimum enclosing circle and
+                    # centroid
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+            print('Object - x = %s \t y = %s\t r = %s' %
+                (int(x) - 320, int(y), int(radius)))
+
+            # only proceed if the radius meets a minimum size
+            if radius > 5:
+
+                # draw the circle and centroid on the frame,
+                # then update the list of tracked points
+                cv2.circle(frame, (int(x), int(y)), int(radius),
+                            (0, 255, 255), 2)
+                cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+                ret, webFrame = cv2.imencode('.jpg',frame)
+
+            # show the frame to our screen
+            #	cv2.imshow("Frame", frame)
+
+        key = cv2.waitKey(1) & 0xFF
+
+        # if the 'q' key is pressed, stop the loop
+        if key == ord("q"):
+            break
+
+        # clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+
+imageProcessing = threading.Thread(target = ImageProcessingThread, args= (0.001, ))
+imageProcessing.start()
 time.sleep(0.1)
 
-# capture frames from the camera
-for image in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        # grab the raw NumPy array representing the image, then initialize the timestamp
-        # and occupied/unoccupied text
-    frame = image.array
-    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+## web app ##
 
-    # construct a mask for the color "green", then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
-    mask = cv2.inRange(hsv, greenLower, greenUpper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
+app = Flask(__name__)
 
-#	cv2.imshow("mask", mask)
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
 
-    # find contours in the mask and initialize the current
-    # (x, y) center of the ball
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    center = None
+def gen():
+    global webFrame
+    """Video streaming generator function."""
+    while True:
+        frame = webFrame
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-    # only proceed if at least one contour was found
-    if len(cnts) > 0:
-                # find the largest contour in the mask, then use
-                # it to compute the minimum enclosing circle and
-                # centroid
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
-        print('Object - x = %s \t y = %s\t r = %s' %
-              (int(x) - 320, int(y), int(radius)))
-
-        # only proceed if the radius meets a minimum size
-        # if radius > 5:
-
-        # draw the circle and centroid on the frame,
-        # then update the list of tracked points
-        # cv2.circle(frame, (int(x), int(y)), int(radius),
-        #            (0, 255, 255), 2)
-        # cv2.circle(frame, center, 5, (0, 0, 255), -1)
-
-        # show the frame to our screen
-        #	cv2.imshow("Frame", frame)
-
-    key = cv2.waitKey(1) & 0xFF
-
-    # if the 'q' key is pressed, stop the loop
-    if key == ord("q"):
-        break
-
-    # clear the stream in preparation for the next frame
-    rawCapture.truncate(0)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True)
